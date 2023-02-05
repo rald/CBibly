ec4980ce110b3cabe6551183c2df11a42ff33362
rald <thecoconutnutisagiantnut@gmail.c>
2023-02-05 15:27:14 +0800

    OK

--- bibly.c
+++ bibly.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#define _GNU_SOURCE
 #include <string.h>
 
 #define BOOK_MAX 256
@@ -21,42 +23,89 @@ char *rmnl(char *s) {
 
 int main(int argc,char **argv) {
 
-  if(argc!=3 && argc!=4) {
+  if(argc!=2 && argc!=3 && argc!=4) {
+    printf("syntax: %s search text\n",argv[0]);
     printf("syntax: %s book chapter [verse]\n",argv[0]);
     return -1;
   }
 
-  char *book=argv[1];
-  int chap=atoi(argv[2]);
-  int vers=0;
-
-  if(argc==4) vers=atoi(argv[3]);
-
   FILE *fp=fopen("kjv.csv","r");
 
-  char *line=NULL;
-  size_t llen=0;
-  ssize_t rlen=0;
+  if(strcasecmp(argv[1],"search")==0) {
 
-  while((rlen=getline(&line,&llen,fp)) && rlen>0) {
+    char *search=argv[2];
+        
+    char *line=NULL;
+    size_t llen=0;
+    ssize_t rlen=0;
 
-    char **tokens=NULL;
-    size_t ntokens=0;
+    while((rlen=getline(&line,&llen,fp)) && rlen>0) {
 
-    tokenize(&tokens,&ntokens,rmnl(line),"|");
+      char **tokens=NULL;
+      size_t ntokens=0;
 
-    char *bname=tokens[0];
-    int cnumber=atoi(tokens[1]);
-    int vnumber=atoi(tokens[2]);
-    char *text=tokens[3];
+      tokenize(&tokens,&ntokens,rmnl(line),"|");
 
-    if(strcasecmp(book,bname)==0 && ((chap==cnumber && vers!=0 && vers==vnumber) || (chap==cnumber && vers==0))) { 
-      printf("%s %d:%d -> %s\n\n",bname,cnumber,vnumber,text);
+      char *bname=tokens[0];
+      int cnumber=atoi(tokens[1]);
+      int vnumber=atoi(tokens[2]);
+      char *text=tokens[3];
+
+      char str[4096];
+
+      sprintf(str,"%s %d:%d %s",bname,cnumber,vnumber,text);
+
+      if(strcasestr(str,search)) { 
+        printf("%s %d:%d -> %s\n\n",bname,cnumber,vnumber,text);
+      }
+
+      free(tokens);
+      tokens=NULL;
+      ntokens=0;
+
+      free(line);
+      line=NULL;
+      llen=0;
     }
 
-    free(line);
-    line=NULL;
-    llen=0;
+    
+  } else {
+
+    char *book=argv[1];
+    int chap=atoi(argv[2]);
+    int vers=0;
+
+    if(argc==4) vers=atoi(argv[3]);
+
+    char *line=NULL;
+    size_t llen=0;
+    ssize_t rlen=0;
+
+    while((rlen=getline(&line,&llen,fp)) && rlen>0) {
+
+      char **tokens=NULL;
+      size_t ntokens=0;
+
+      tokenize(&tokens,&ntokens,rmnl(line),"|");
+
+      char *bname=tokens[0];
+      int cnumber=atoi(tokens[1]);
+      int vnumber=atoi(tokens[2]);
+      char *text=tokens[3];
+
+      if(strcasecmp(book,bname)==0 && ((chap==cnumber && vers!=0 && vers==vnumber) || (chap==cnumber && vers==0))) { 
+        printf("%s %d:%d -> %s\n\n",bname,cnumber,vnumber,text);
+      }
+
+      free(tokens);
+      tokens=NULL;
+      ntokens=0;
+
+      free(line);
+      line=NULL;
+      llen=0;
+    }
+
   }
   
   return 0;
